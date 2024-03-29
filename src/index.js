bc087191a4ad98e864d6fd6f7cd6a63ddc173e0a
Amro Abdelfatah <amro.utp.pe@gmail.com>
2024-03-29 01:47:35 +0100

    lesson(functional programming): compose & pipe

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Redux Starter Project!!");
+import { compose, pipe } from "lodash/fp";
+
+const trim = (str) => str.trim();
+const convertToUpperCase = (str) => str.toUpperCase();
+const generateMessage = (str) => `Hello, ${str}! Good morning!`;
+
+const resultWithChaining = generateMessage(
+  convertToUpperCase(trim("   john   "))
+); // hard to read
+console.log(resultWithChaining);
+
+// simple solution from loadash
+// from right to left => compose
+const resultWithCompose = compose(generateMessage, convertToUpperCase, trim);
+// from left to right => pipe
+const resultWithPipe = pipe(trim, convertToUpperCase, generateMessage);
+
+console.log(resultWithCompose("   john   "));
+console.log(resultWithPipe("   john   "));
