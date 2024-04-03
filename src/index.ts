63b56061ed50286049c8ec6c79b83437f3d7b087
Amro Abdelfatah <amro.utp.pe@gmail.com>
2024-04-03 02:27:06 +0200

    Lesson(redux store): subscribe method

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { addTask, removeTask } from "./actions";
 import store from "./store";
 
+// subscribe method runs whenever the store state is updated
+store.subscribe(() => {
+  console.log("Updated!", store.getState());
+});
+
 store.dispatch(addTask("Learn Redux"));
 store.dispatch(addTask("Learn TS"));
 
-console.log(store.getState());
+// console.log(store.getState());
 
 store.dispatch(removeTask(1));
-console.log(store.getState());
+// console.log(store.getState());
